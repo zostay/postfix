b8fef88bd84f8494164e3f6e07cbb84c87348338
Sterling Hanenkamp <sterling@hanenkamp.com>
2018-01-22 11:37:03 -0600

    I need more help here

--- start-postfix.sh
+++ start-postfix.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-cp /usr/local/share/postfix/postfix-files /etc/postfix/postfix-files
+cp -v /usr/local/share/postfix/postfix-files /etc/postfix/postfix-files
 apt-get --fix-broken install
 
+ls -la /usr/local/share/postfix
+echo ----------------------------------------------------------------
 ls -la /etc/postfix
 
 postfix start && journalctl -f
